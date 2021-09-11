98f4ac6a786ead350854edcbe5a724c198bea6a7
Kongxib Vue <kongcvue@gmail.com>
2021-09-11 13:47:06 -0500

    added additional code for cleaner look

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -5,6 +5,7 @@ console.log('***** Cart Functions *****');
 
 //create global variable Basket
 let basket =[];
+const maxItems = 5;//max items variable
 
 function addItem(item, arr){
   arr.push(item);
@@ -24,18 +25,28 @@ console.log(addItem('Lord of the Rings Triology - Blue Ray Edition', basket));
 console.log(addItem('Pantene Shampoo/Conditioner Set', basket));
 console.log(addItem('Doritos Cool Ranch', basket));
 
-function listItems(){
+function listItems(arr){
+  let counter = 1;
   //function to show all items in basket array
-  for(item in basket){
+  for(item in arr){
     //for reach value in basket array, console.log it
-    console.log(item);
+    console.log('Item', counter++, ': ' ,arr[item]);
   }
 }
 
+listItems(basket);
 //testing 'listItems' code
 
-
-function empty(){
-  basket.splice(0, basket.length);
-  //deletes all items from index 0 upwards
+function empty(array){
+  array.splice(0, array.length);
+  console.log('Emptying array...');
+  if(array.length === 0){
+    console.log('Array is empty!');  //deletes all items from index 0 to end of basket
+  } //spent some time trying to print 'basket' into the console, but it wouldn't let me
+  //after having spent some time on stackoverflow i understand that it's only referenced, can't actually be called
+  //bummed out as I'm trying to make it as visually pleasing as possible and convenient for other features
+ //I'm probably making this more complicated than needed to be, wanted to have the function be multi-use
 }
+
+empty(basket);
+//empty the basket

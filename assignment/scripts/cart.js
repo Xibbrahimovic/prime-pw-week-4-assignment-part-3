98d319b790fe884a9c708fca54008b0d1add5dd7
Kongxib Vue <kongcvue@gmail.com>
2021-09-11 12:14:24 -0500

    finally logging true after adding items

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -6,13 +6,13 @@ console.log('***** Cart Functions *****');
 //create global variable Basket
 let basket =[];
 
-function addItem(item, basket){
-  basket.push(item);
+function addItem(item, arr){
+  arr.push(item);
   //adds item to the basket array
-  for(let i = 0; i <basket.length; i++){
+  for(let i = 0; i <arr.length; i++){
     if(basket[i] === item) {
       console.log('Adding','"', item, '"', 'to your basket!');
-      console.log('Your Prime basket inventory: ', basket);
+      console.log('Your Prime basket inventory: ', arr);
       //checking to see if item exists in array (added), if so, return true
     return true;
   }
@@ -20,9 +20,9 @@ function addItem(item, basket){
 }
 
 //testing function "addItem" code
-addItem('Lord of the Rings Triology - Blue Ray Edition', basket);
-addItem('Pantene Shampoo/Conditioner Set', basket);
-addItem('Doritos Cool Ranch', basket);
+console.log(addItem('Lord of the Rings Triology - Blue Ray Edition', basket));
+console.log(addItem('Pantene Shampoo/Conditioner Set', basket));
+console.log(addItem('Doritos Cool Ranch', basket));
 
 function listItems(){
   //function to show all items in basket array
